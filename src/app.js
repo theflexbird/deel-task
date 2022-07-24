e7ec90ed7af3ccbc81346836c32cf7d02ba0b096
Mateusz <mateusz@whitebird.pl>
2022-07-24 13:40:59 +0200

    /balances/deposit/ implementation

--- src/app.js
+++ src/app.js
@@ -98,6 +98,43 @@ app.post("/jobs/:job_id/pay", getProfile, async (req, res) => {
   res.json(job);
 });
 
+app.post("/balances/deposit/:userId", getProfile, async (req, res) => {
+  const { profile } = req;
+  const { userId } = req.params;
+  const { deposit } = req.body;
+  if (parseInt(userId) !== profile.id) return res.status(400).end();
+  const { Job, Profile, Contract } = req.app.get("models");
+  const totalAmount = await Job.findOne({
+    attributes: [
+      [sequelize.fn('COALESCE', sequelize.fn("sum", sequelize.col("price")), 0), "amount"],
+    ],
+    where: {
+      paid: { [Op.not]: true },
+    },
+    raw: true,
+    include: {
+      attributes: [],
+      model: Contract,
+      where: {
+        status: { [Op.not]: "terminated" }
+      },
+      include: {
+        model: Profile,
+        as: "Client",
+        where: {
+          id: userId
+        }
+      }
+    }
+  });
+  const AllowedAmount = totalAmount.amount * 25 / 100;
+  if (AllowedAmount < deposit) return res.status(403).end();
+  // save to database
+  profile.balance = profile.balance + deposit;
+  profile.save();
+  res.json(profile);
+});
+
 app.get("/admin/best-profession", async (req, res) => {
   const { start, end } = req.query;
   if (!start || !end) return res.status(400).end();
